df6bf288347ee6ae290903479b990f6e15dc389b
jinyoungk1208 <jinyoungk1208@gmail.com>
2022-01-02 23:05:46 +0900

    component 분리, fragnent 문법

--- src/App.js
+++ src/App.js
@@ -29,7 +29,21 @@ function App() {
         <p>22.01.01</p>
         <hr />
       </div>
+
+      <Modal />
     </div>
+  );
+}
+
+function Modal() {
+  return (
+    <>
+      <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>내용</p>
+      </div>
+    </>
   );
 }
 
