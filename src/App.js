69e693af8f2201c4ded0d6116849c657d54f175e
jinyoungk1208 <jinyoungk1208@gmail.com>
2022-01-08 17:06:16 +0900

    props, prettier 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 /* eslint-disable */
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  let [title, changeTitle] = useState(['글 제목 1', '글 제목 2', '글 제목 3']);
+  let [title, changeTitle] = useState(["글 제목 1", "글 제목 2", "글 제목 3"]);
   let [count, changeCount] = useState(0);
   let [modal, changeModal] = useState(false);
 
   function cbChangeTitle() {
     let newArray = [...title];
-    newArray[0] = '글 수정 1';
+    newArray[0] = "글 수정 1";
     changeTitle(newArray);
   }
 
@@ -18,7 +18,7 @@ function App() {
       <div className="black-nav">
         <div>개발 블로그</div>
       </div>
-      <button onClick={ cbChangeTitle }>수정</button>
+      <button onClick={cbChangeTitle}>수정</button>
       {/*<div className="list">
         <h4 onClick={ () => { changeModal(true); } }>{ title[0] } <span onClick={ () => { changeCount(count+1); } }>👍</span>{ count }</h4>
         <p>22.01.01</p>
@@ -34,33 +34,47 @@ function App() {
         <p>22.01.01</p>
         <hr />
       </div>*/}
-      {
-        title.map((item) => {
-          return (
-            <div className="list">
-              <h4 onClick={ () => { changeModal(!modal); } }>{ item } <span onClick={ () => { changeCount(count+1); } }>👍</span>{ count }</h4>
-              <p>22.01.01</p>
-              <hr />
-            </div>
-          );
-        })
-      }
+      {title.map((item) => {
+        return (
+          <div className="list">
+            <h4
+              onClick={() => {
+                changeModal(!modal);
+              }}
+            >
+              {item}{" "}
+              <span
+                onClick={() => {
+                  changeCount(count + 1);
+                }}
+              >
+                👍
+              </span>
+              {count}
+            </h4>
+            <p>22.01.01</p>
+            <hr />
+          </div>
+        );
+      })}
 
-      <button onClick={ () => { changeModal(!modal); } }>모달창</button>
-      {
-        modal === true
-        ? <Modal />
-        : null
-      }
+      <button
+        onClick={() => {
+          changeModal(!modal);
+        }}
+      >
+        모달창
+      </button>
+      {modal === true ? <Modal title={title} /> : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <>
       <div className="modal">
-        <h2>제목</h2>
+        <h2>{props.title[0]}</h2>
         <p>날짜</p>
         <p>내용</p>
       </div>
